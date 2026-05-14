4e8c71c2acf80058f593953c0405261954464f05
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2026-05-14 06:43:31 +0545

    Add files via upload
    5-14

---
+++ onlineshopping/assets/js/main.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var swiper = new Swiper(".mySwiper1", {
+      
+      spaceBetween: 30,
+       autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+    });
